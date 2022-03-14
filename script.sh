70fb57ef210ac58346067c203fa4709402fe0c1f
mazdak nazemi <nazemi.works@gmail.com>
2022-03-14 16:19:57 +0100

    more on , updating script

--- script.sh
+++ script.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
 echo "VERCEL_ENV: $VERCEL_ENV"
-echo "BRANCH_NAME$(git branch --show-current)"
+echo "BRANCH_NAME: $(git branch --show-current)"
 
-if [[ $(git branch --show-current) == 'mazdak']] ; then
-  echo "master must be build not other branc"
-  exit 0;
+if [[ $(git branch --show-current) == 'mazdak' ]] ; then
+  # Proceed with the build
+  echo "🛑  branch is mazdak"
+  exit 1;
+
+else
+  # Don't build
+  echo "HOW ARE YOU!!!!!!"
 fi
 
 if [[ "$VERCEL_ENV" == "production" ]] ; then
+  # Proceed with the build
   echo "✅ - Build can proceed"
-  echo " I am HERE "
+  exit 1;
+
 else
   # Don't build
   echo "🛑 - Build cancelled"
