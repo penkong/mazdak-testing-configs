7fcc5be72218819bb8b454a3d3d9c03219660b8e
mazdak nazemi <nazemi.works@gmail.com>
2022-03-14 16:15:39 +0100

    more on make script works

--- script.sh
+++ script.sh
@@ -3,16 +3,16 @@
 echo "VERCEL_ENV: $VERCEL_ENV"
 echo "BRANCH_NAME$(git branch --show-current)"
 
+if [[ $(git branch --show-current) == 'mazdak']] 
+then
+  echo "master must be build not other branc"
+  exit 0;
+fi
+
 if [[ "$VERCEL_ENV" == "production" ]]  
 then
-  if [[ $(git branch --show-current) == 'mazdak']] 
-  then
-    echo "master must be build not other branc"
-    exit 0;
-  else 
-    echo "✅ - Build can proceed"
-    echo " I am HERE "
-  fi
+  echo "✅ - Build can proceed"
+  echo " I am HERE "
 else
   # Don't build
   echo "🛑 - Build cancelled"
